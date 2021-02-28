48bbdf32859a5ac1841d29173bf7a1a386422ffb
Burak Tunc <burak.tunc@check24.de>
2021-02-28 15:58:52 +0100

    updates

--- RazeCoreFramework/Tests/RazeCoreFrameworkTests/RazeCoreFrameworkTests.swift
+++ RazeCoreFramework/Tests/RazeCoreFrameworkTests/RazeCoreFrameworkTests.swift
@@ -2,14 +2,12 @@ import XCTest
 @testable import RazeCoreFramework
 
 final class RazeCoreFrameworkTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RazeCoreFramework().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testColorRedEqual(){
+            let color = RazeCoreFramework.colorFromHexString("FF0000")
+            XCTAssertEqual(color, .red)
+        }
+        
+        static var allTests = [
+            ("testColorRedEqual", testColorRedEqual),
+        ]
 }
