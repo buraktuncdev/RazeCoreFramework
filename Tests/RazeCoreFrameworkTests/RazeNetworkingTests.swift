85000c88a30bbfc6f3703f5313f90ee85948abc3
Burak Tunc <burak.tunc@check24.de>
2021-02-28 20:19:03 +0100

    Refactored Networking Architecture and Description added

--- Tests/RazeCoreFrameworkTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreFrameworkTests/RazeNetworkingTests.swift
@@ -8,17 +8,30 @@
 import XCTest
 @testable import RazeCoreFramework
 
+class NetworkingSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
 final class RazeNetworkingTests: XCTestCase {
     
     func testLoadDataCell(){
         let manager = RazeCoreFramework.Networking.Manager()
+        let session = NetworkingSessionMock()
+        manager.session = session // MARK: MOCK SESSION WITH PROPERTY INJECTION
         let expectation = XCTestExpectation(description: "Called For Data")
-        guard let url = URL(string: "https://raywenderlich.com") else { return XCTFail("Could not create URL properly")}
+        let data = Data([0,1,0,1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
         manager.loadData(from:url){ result in
             expectation.fulfill()
             switch result {
                 case .success(let returnedData):
-                    XCTAssertNotNil(returnedData, "Response Data is Nil")
+                    XCTAssertEqual(data, returnedData, "manager returned unexpected data")
                 case .failure(let error):
                     XCTFail(error?.localizedDescription ?? "error forming error result")
             }
